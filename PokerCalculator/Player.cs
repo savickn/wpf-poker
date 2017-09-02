f793e99f0f35b0cd1407769ac8b1b2a8b9a2dcdb
Nick Savickas <nick.savickas@gmail.com>
2017-09-01 23:54:29 -0400

    game logic

--- PokerCalculator/Player.cs
+++ PokerCalculator/Player.cs
@@ -61,17 +61,46 @@ namespace PokerCalculator {
             return br;
         }
 
-        public List<Option> populateActions(state) {
-            List<Option> opts = { Option.FOLD }
-
-
+        public List<Option> populateActions(PotState state) {
+            List<Option> opts = new List<Option> { Option.FOLD };
+            if (state.currentBet == state.playerContribution) {
+                opts.Add(Option.CHECK);
+            }
+            if (state.currentBet > state.playerContribution) {
+                opts.Add(Option.CALL);
+            }
+            if (state.currentBet >= state.playerContribution) {
+                opts.Add(Option.RAISE);
+            }
             return opts;
         }
 
-        public Action selectAction(state) {
+        public Action selectAction(GameState gs, PotState ps) {
+            List<Option> opts = this.populateActions(ps);
 
+            /*string prompt = "Select an action: ";
+            foreach(Option o in opts) {
+                prompt += String.Format("{0} - {1}", opts.IndexOf(o), o); 
+            }
 
+            int selection;
+            while(true) {
+                selection = (int)Console.ReadLine(prompt);
+            }*/
 
+            Option selection = opts[Console.Read()];
+
+            Action action;
+            if(selection == Option.FOLD) {
+                action = new Fold();
+            } else if(selection == Option.CHECK) {
+
+            } else if(selection == Option.CALL) {
+
+            } else if(selection == Option.RAISE){
+
+            }
+            return action;
         }
 
         ///// UTILITY METHODS /////
--- PokerCalculator/PotState.cs
+++ PokerCalculator/PotState.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 namespace PokerCalculator {
     class PotState {
         public double pot { get; }
+        public Street street { get; }
         public double currentBet { get; }
         public double playerContribution { get; }
 
-        public PotState(double pot, double currentBet, double contribution) {
+        public PotState(double pot, Street street, double currentBet, double contribution) {
             this.pot = pot;
+            this.street = street;
             this.currentBet = currentBet;
             this.playerContribution = contribution;
         }
-
     }
 }
